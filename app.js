50210309c73c079e5933ad924ae9e060d019dbfa
bcrazydreamer <31788510+bcrazydreamer@users.noreply.github.com>
2019-02-05 01:54:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ var express =           require("express");
 var app =               express();
 var port =              process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next){
   var htmltxt = '<center><h1>Welcome</h1></center>';
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(htmltxt);
 });
 
-app.get('/voice.xml', function(req, res){
+app.get('/voice.xml', function(req, res, next){
   filetxt  = '<?xml version="1.0" encoding="UTF-8"?>';
   filetxt += '<Response>';
   filetxt += '<Say voice="alice">Hello we are from IPE Teacher Learning Program. This call is to inform you that you are invited for third phase'
