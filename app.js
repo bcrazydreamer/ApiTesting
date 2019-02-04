25223613b9ac0844ea45f3a503d8d3b6a5aaf038
bcrazydreamer <31788510+bcrazydreamer@users.noreply.github.com>
2019-02-05 02:59:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express =           require("express");
 var app =               express();
 var port =              process.env.PORT || 3000;
+var path =              require('path');
+app.use(express.static(path.join(__dirname,"public")));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
