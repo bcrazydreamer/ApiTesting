f067ddb41d81406f941420aa91f692dcf6986cea
bcrazydreamer <31788510+bcrazydreamer@users.noreply.github.com>
2019-02-05 01:32:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
   res.end(htmltxt);
 });
 
-app.get('/voicedemo', function(req, res){
+app.get('/voice.xml', function(req, res){
   filetxt  = '<Response>';
   filetxt += '<Say voice="alice">Hello we are from IPE Teacher Learning Program. This call is to inform you that you are invited for third phase'
   filetxt += ' teacher learning trainning. We will sortly call you regarding your confirmation. Thankyou and have a nice day.</Say>';
