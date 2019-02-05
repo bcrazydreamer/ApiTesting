03effa5c3fa67fc1f032ad0fe080a05d0ee84d00
bcrazydreamer <31788510+bcrazydreamer@users.noreply.github.com>
2019-02-05 12:08:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,25 +16,29 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function(req, res, next){
-    var htmltxt = '<center><h1>Make Call</h1></center>';
-    htmltxt += '<br><center><a href="/makeCall">Make Call</a></center>';
+    var htmltxt ='<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head><body>'
+    htmltxt += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">';
+    htmltxt += '<center><h1>Make Call</h1></center>';
+    htmltxt += '<br><center><a href="/makeCall" class="btn btn-primary">Make Call</a></center></body></head>';
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(htmltxt);
 });
 
 app.get('/makeCall', function(req, res, next){
-  var htmltxt = '<center><h1>Calling...</h1></center>';
-  htmltxt += '<br><center><a href="/">Back</a></center>';
-  client.calls
-      .create({
-         url: 'https://bcrazytesting.herokuapp.com/voice.xml',
-         to: '+918708372614',
-         from: '+18143249002'
-       })
-      .then(call => console.log(call.sid))
-      .done();
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(htmltxt);
+    var htmltxt ='<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head><body>'
+    htmltxt += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">';
+    htmltxt += '<center><h1>Calling...</h1></center>';
+    htmltxt += '<br><center><a href="/" class="btn btn-danger">Back</a></center></body></html>';
+    client.calls
+        .create({
+            url: 'https://bcrazytesting.herokuapp.com/voice.xml',
+            to: '+918708372614',
+            from: '+18143249002'
+        })
+        .then(call => console.log(call.sid))
+        .done();
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(htmltxt);
 });
 
 app.post('/voice.xml', function(req, res, next){
